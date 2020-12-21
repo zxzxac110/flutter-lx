f1d6513ad13a8f21acbd580c295d3aebfd14672f
lishihao <lishihao@example.com>
2020-12-21 14:28:22 +0800

    无题

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -6,6 +6,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  // 表单状态
+  GlobalKey _formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,15 +16,57 @@ class _LoginState extends State<Login> {
         title: Text('登录'),
         toolbarHeight: 48.0,
       ),
-      body: Wrap(
-        children: [
-          new Image.asset(
-            'images/login.png',
-            width: 600.0,
-            height: 240.0,
-            fit: BoxFit.cover,
-          ),
-        ],
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+        child: ListView(
+          children: [
+            _topLogo(),
+            _inputTextArea(),
+            Text('确定'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _topLogo() {
+    return Image.asset(
+      'images/login.png',
+      width: 128.0,
+      height: 128.0,
+      alignment: Alignment.center,
+      fit: BoxFit.contain,
+    );
+  }
+
+  Widget _inputTextArea() {
+    return Container(
+      margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          color: Color(0xffeeeeee)),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+
+            ),
+            TextFormField(
+              //   decoration: InputDecoration(
+              //     icon: Icon(Icons.user,)
+              //   ),
+              // hintText
+            ),
+            TextFormField(),
+            TextFormField(),
+            Text('账号'),
+            Text('密码'),
+            Text('图像验证码'),
+            Text('短信验证码'),
+          ],
+        ),
       ),
     );
   }
